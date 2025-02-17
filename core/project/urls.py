5b6645ee95a1cea887f3f349de2506bb254c13bc
edmundrotimi <edmundrotimi@gmail.com>
2025-02-17 11:48:52 +0200

    project url paths

--- core/project/urls.py
+++ core/project/urls.py
@@ -1,6 +1,21 @@
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
+
+from core.project.settings import DEBUG  # type: ignore
+from core.project.settings import MEDIA_ROOT, MEDIA_URL  # type: ignore
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
+	path('api/', include('core.api.urls')),
+	path('favicon.ico', RedirectView.as_view(url='/static/assets/favicon/favicon.ico')),
 ]
+
+# Add paths in debug mode
+if DEBUG:
+	import debug_toolbar
+
+	urlpatterns += [
+		path('__debug__/', include(debug_toolbar.urls)),
+	] + static(MEDIA_URL, document_root=MEDIA_ROOT)
