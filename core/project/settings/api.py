51b0a3d995dbad64d37873d696ede2af4c1bc1d8
edmundrotimi <edmundrotimi@gmail.com>
2025-02-17 11:49:28 +0200

    update api settings config

--- core/project/settings/api.py
+++ core/project/settings/api.py
@@ -1,11 +1,3 @@
-from django.utils.timezone import timedelta
-
-Simple_JWT = {
-	'ACCESS_TOKEN_LIFETIME': timedelta(minutes=35),
-	'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-	'ALGORITHM': 'HS512',
-}
-
 SWAGGER_SETTINGS = {
 	'USE_SESSION_AUTH': False,
 	'SECURITY_DEFINITIONS': {'Bearer': {'type': 'apiKey', 'name': 'Authorization', 'in': 'header'}},
