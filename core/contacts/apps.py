34b2f8ba81fe1cf2a8e66f595268132b1ef88097
edmundrotimi <edmundrotimi@gmail.com>
2025-02-17 11:44:15 +0200

    contact model

--- core/contacts/apps.py
+++ core/contacts/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class ContactsConfig(AppConfig):
-	default_auto_field = 'django.db.models.BigAutoField'
-	name = 'core.contacts'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'core.contacts'
+    icon = 'fas fa-address-book'
+    divider_title = "Apps"
+    priority = 0
+    hide = False
--- core/contacts/models.py
+++ core/contacts/models.py
@@ -1,3 +1,26 @@
-# from django.db import models
+from django.db import models
+from django.utils import timezone
+import pgcrypto
+from simple_history.models import HistoricalRecords
 
-# Create your models here.
+from core.testimonies.models import generate_custom_id
+
+
+class Contacts(models.Model):
+	id = models.CharField(  # noqa: A003
+		default=generate_custom_id, editable=False, unique=True, max_length=15, primary_key=True
+	)
+	full_name = pgcrypto.EncryptedCharField(max_length=100)
+	phone = pgcrypto.EncryptedCharField(max_length=254)
+	message = pgcrypto.EncryptedTextField(max_length=500)
+	has_read = models.BooleanField(default=False, help_text='admin has read message')
+	history = HistoricalRecords()
+	date_received = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+		return self.full_name
+
+	class Meta:
+		verbose_name = 'Contact'
+		verbose_name_plural = 'Contacts'
+		ordering = ['-date_received']
