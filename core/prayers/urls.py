14efac3a880115dde25e488411c60a5556659dcc
edmundrotimi <edmundrotimi@gmail.com>
2025-02-27 01:23:11 +0200

    linting

--- core/prayers/urls.py
+++ core/prayers/urls.py
@@ -1,5 +1,6 @@
-from .views import PrayerRequestPage
 from django.urls import path
+
+from .views import PrayerRequestPage
 
 urlpatterns = [
 	path('request/form/', PrayerRequestPage.as_view(), name='prayer_request'),
