02e45af5df794cc4bc0bf9982fddfb68c404d5b1
edmundrotimi <edmundrotimi@gmail.com>
2025-02-26 18:40:52 +0200

    update models

--- core/testimonies/models.py
+++ core/testimonies/models.py
@@ -14,14 +14,27 @@ def generate_custom_id():
 	return custom_id
 
 
+designation_choices = (
+	('Mr', 'Mr'),
+	('Mrs', 'Mrs'),
+	('Deacon', 'Deacon'),
+	('Deaconess', 'Deaconess'),
+)
+
+
 class Testimony(models.Model):
 	id = models.CharField(  # noqa: A003
-		default=generate_custom_id, editable=False, unique=True, max_length=15, primary_key=True
+		default=generate_custom_id, editable=False, unique=True, max_length=20, primary_key=True
 	)
 	first_name = pgcrypto.EncryptedCharField(max_length=100)
 	last_name = pgcrypto.EncryptedCharField(max_length=100)
-	designation = pgcrypto.EncryptedCharField(max_length=254)
-	tesitmony = pgcrypto.EncryptedTextField(max_length=800)
+	designation = pgcrypto.EncryptedCharField(max_length=254, choices=designation_choices, default='Mrs')
+	title = pgcrypto.EncryptedTextField(max_length=200, null=True, blank=True)
+	testimony = pgcrypto.EncryptedTextField(max_length=1000)
+	location = pgcrypto.EncryptedCharField(
+		max_length=200, null=True, blank=True, verbose_name='Church Branch/Location'
+	)
+	publish = models.BooleanField(default=True)
 	history = HistoricalRecords()
 	date_received = models.DateTimeField(default=timezone.now)
 
