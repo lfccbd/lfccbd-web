9dc3b1720579b39eee06ca3781cc7c73a7e6bd67
edmundrotimi <edmundrotimi@gmail.com>
2025-03-01 18:41:43 +0200

    linting

--- core/project/settings/development.py
+++ core/project/settings/development.py
@@ -11,38 +11,39 @@ DEBUG = DEBUG
 
 # Database
 DATABASES = {
-	'default': {
-		'ENGINE': ENV.config('ENGINE'),
-		'NAME': ENV.config('NAME'),
-		'HOST': ENV.config('HOST'),
-		'USER': ENV.config('USER'),
-		'PASSWORD': ENV.config('PASSWORD'),
-		'PORT': ENV.config('PORT'),
-	}
+    'default': {
+        'ENGINE': ENV.config('ENGINE'),
+        'NAME': ENV.config('NAME'),
+        'HOST': ENV.config('HOST'),
+        'USER': ENV.config('USER'),
+        'PASSWORD': ENV.config('PASSWORD'),
+        'PORT': ENV.config('PORT'),
+    }
 }
 
 # Add django extension and debug toolbar to installed apps
 if 'django_extensions' not in INSTALLED_APPS and 'debug_toolbar' not in INSTALLED_APPS:
-	INSTALLED_APPS.insert(len(INSTALLED_APPS) - 2, 'django_extensions')
-	INSTALLED_APPS.insert(len(INSTALLED_APPS) - 3, 'debug_toolbar')
+    INSTALLED_APPS.insert(len(INSTALLED_APPS) - 2, 'django_extensions')
+    INSTALLED_APPS.insert(len(INSTALLED_APPS) - 3, 'debug_toolbar')
 
 # Django debug middleware
 if 'debug_toolbar.middleware.DebugToolbarMiddleware' not in MIDDLEWARE:
-	MIDDLEWARE.insert(len(MIDDLEWARE) - 1, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+    MIDDLEWARE.insert(
+        len(MIDDLEWARE) - 1, 'debug_toolbar.middleware.DebugToolbarMiddleware'
+    )
 
 # set internal ips
 INTERNAL_IPS = ['127.0.0.1']
 
 # Cors settings
 CORS_ALLOWED_ORIGINS = [
-	'http://localhost:8000',
+    'http://localhost:8000',
 ]
 
 
 # Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = f'{BASE_DIR}/core/media'
-
 
 # Captcha settings
 RECAPTCHA_PUBLIC_KEY = ENV.config('RECAPTCHA_PUBLIC_KEY')
