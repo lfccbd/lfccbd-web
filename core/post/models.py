c6b14be951b547eb52de5fef1b24bd6962311026
edmundrotimi <edmundrotimi@gmail.com>
2025-04-24 15:12:41 +0200

    update model

--- core/post/models.py
+++ core/post/models.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 from django_prose_editor.fields import ProseEditorField
 from simple_history.models import HistoricalRecords
 
-from core.resources.models import Media
 from core.testimonies.models import generate_custom_id
 
 
@@ -113,121 +112,6 @@ class VerseOfDay(models.Model):
 	class Meta:
 		verbose_name = 'Verse Of The Day'
 		verbose_name_plural = 'Verse Of The Day'
-		ordering = ['-date_created']
-
-
-class BookmarkedUpcomingService(models.Model):
-	bookmark_id = models.CharField(  # noqa: A003
-		default=generate_custom_id,
-		editable=False,
-		unique=True,
-		max_length=20,
-		primary_key=True,
-	)
-	item_bookmarked = models.ForeignKey('UpcomingService', on_delete=models.CASCADE)
-	history = HistoricalRecords()
-	publish = models.BooleanField(default=True, help_text='make visible on site')
-	date_created = models.DateTimeField(auto_now_add=True)
-	last_updated = models.DateTimeField(default=timezone.now)
-
-	def __str__(self):
-		return self.item_bookmarked.service_theme
-
-	class Meta:
-		verbose_name = 'Bookmarked Upcoming Service'
-		verbose_name_plural = 'Bookmarked Upcoming Service'
-		ordering = ['-date_created']
-
-
-class BookmarkedEpistleOfMonth(models.Model):
-	bookmark_id = models.CharField(  # noqa: A003
-		default=generate_custom_id,
-		editable=False,
-		unique=True,
-		max_length=20,
-		primary_key=True,
-	)
-	item_bookmarked = models.ForeignKey('EpistleOfMonth', on_delete=models.CASCADE)
-	history = HistoricalRecords()
-	publish = models.BooleanField(default=True, help_text='make visible on site')
-	date_created = models.DateTimeField(auto_now_add=True)
-	last_updated = models.DateTimeField(default=timezone.now)
-
-	def __str__(self):
-		return self.item_bookmarked.epistle_message_theme
-
-	class Meta:
-		verbose_name = 'Bookmarked Epistle Of The Month'
-		verbose_name_plural = 'Bookmarked Epistle Of The Month'
-		ordering = ['-date_created']
-
-
-class BookmarkedWordOfDay(models.Model):
-	bookmark_id = models.CharField(  # noqa: A003
-		default=generate_custom_id,
-		editable=False,
-		unique=True,
-		max_length=20,
-		primary_key=True,
-	)
-	item_bookmarked = models.ForeignKey('WordOfDay', on_delete=models.CASCADE)
-	history = HistoricalRecords()
-	publish = models.BooleanField(default=True, help_text='make visible on site')
-	date_created = models.DateTimeField(auto_now_add=True)
-	last_updated = models.DateTimeField(default=timezone.now)
-
-	def __str__(self):
-		return self.item_bookmarked.message_topic
-
-	class Meta:
-		verbose_name = 'Bookmarked Word Of The Day'
-		verbose_name_plural = 'Bookmarked Word Of The Day'
-		ordering = ['-date_created']
-
-
-class BookmarkedVerseOfDay(models.Model):
-	bookmark_id = models.CharField(  # noqa: A003
-		default=generate_custom_id,
-		editable=False,
-		unique=True,
-		max_length=20,
-		primary_key=True,
-	)
-	item_bookmarked = models.ForeignKey('WordOfDay', on_delete=models.CASCADE)
-	history = HistoricalRecords()
-	publish = models.BooleanField(default=True, help_text='make visible on site')
-	date_created = models.DateTimeField(auto_now_add=True)
-	last_updated = models.DateTimeField(default=timezone.now)
-
-	def __str__(self):
-		return self.item_bookmarked.verse
-
-	class Meta:
-		verbose_name = 'Bookmarked Verse Of The Day'
-		verbose_name_plural = 'Bookmarked Verse Of The Day'
-		ordering = ['-date_created']
-
-
-class SaveForLaterMedia(models.Model):
-	id = models.CharField(  # noqa: A003
-		default=generate_custom_id,
-		editable=False,
-		unique=True,
-		max_length=20,
-		primary_key=True,
-	)
-	media_saveded = models.ForeignKey(Media, on_delete=models.CASCADE)
-	history = HistoricalRecords()
-	publish = models.BooleanField(default=True, help_text='make visible on site')
-	date_created = models.DateTimeField(auto_now_add=True)
-	last_updated = models.DateTimeField(default=timezone.now)
-
-	def __str__(self):
-		return self.item_bookmarked.file_title
-
-	class Meta:
-		verbose_name = 'Saved Media'
-		verbose_name_plural = 'Saved Media'
 		ordering = ['-date_created']
 
 
