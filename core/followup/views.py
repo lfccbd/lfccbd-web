82f9eebd4abbf00a15fc93349d112a0405b21b62
edmundrotimi <edmundrotimi@gmail.com>
2025-04-25 10:51:15 +0200

    update followup view and add outreach view

--- core/followup/views.py
+++ core/followup/views.py
@@ -1,119 +1,113 @@
 from django.contrib import messages
-from django.http import HttpResponsePermanentRedirect
-from django.shortcuts import render
-from django.urls import reverse, reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views import View
+from django.views.generic.edit import CreateView
 
 from core.followup.models.membership import MemberFollowUp
 from core.followup.models.outreach import FollowUpOutreach
 
-from .forms import FollowUpCheckupForm, FollowUpMembershipForm, FollowUpOutreachForm  # type: ignore
+from .forms import FollowUpCheckupForm, FollowUpMembershipForm, FollowUpOutreachForm, OutreachForm  # type: ignore
 
 
-class CheckupView(View):
-    success_url = 'followup'
+class CheckupView(LoginRequiredMixin, View):
     template_name = 'forms/followup.html'
+    success_url_name = 'followup'
+    login_url = reverse_lazy('account_login')
+    redirect_field_name = 'next'
 
-    def get(self, request, *_args, **kwargs):
+    def _get_context_data(self, **kwargs):
         context = {
             'checkup_form': FollowUpCheckupForm(),
             'sunday_form': FollowUpMembershipForm(),
             'outreach_form': FollowUpOutreachForm(),
-            'form': {'error': ''},
         }
-        return render(request, self.template_name, context)
 
-    def post(self, request, *_args, **kwargs):
+        # allow extra context to be passed in
+        context.update(kwargs)
+        return context
 
-        if self.request.POST.get('form_section', None) == 'checkup':
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self._get_context_data())
+
+    def post(self, request, *args, **kwargs):
+        form_section = request.POST.get('form_section', None)
+        active_form_class = None
+        message_text = ''
+        form_key = ''
+
+        if form_section == 'checkup':
             active_form_class = FollowUpCheckupForm
-            selected_form_section = 'form_section_checkup'
+            form_key = 'checkup_form'
             message_text = 'Checkup Form Saved Successfully.'
-
-        elif self.request.POST.get('form_section', None) == 'sunday':
+        elif (
+            form_section == 'sunday_first_timer' or form_section == 'sunday_new_convert'
+        ):
             active_form_class = FollowUpMembershipForm
-            if self.request.POST.get('followup_type', None) == 'Firt Timer':
-                selected_form_section = 'form_section_sunday_timer'
-            else:
-                selected_form_section = 'form_section_sunday_convert'
+            form_key = 'sunday_form'
             message_text = 'Follow-Up Form Saved Successfully.'
-
-        else:
-            # active form class
+        elif form_section == 'outreach':
             active_form_class = FollowUpOutreachForm
-            selected_form_section = 'form_section_outreach'
+            form_key = 'outreach_form'
             message_text = 'Outreach Form Saved Successfully.'
 
-        # copy the request
-        form_data = request.POST.copy()
-
-        # use modified request with form class
-        form = active_form_class(form_data)
+        form = active_form_class(request.POST)
 
         if form.is_valid():
-            # get phone number
-            phone_number = self.request.POST.get('phone_number', None)
+            instance = form.save(commit=False)
 
-            if active_form_class == FollowUpOutreachForm:
-                cleaned_data = form.cleaned_data
+            # add user only if the form's model has a user field
+            if hasattr(instance, 'user'):
+                instance.user = request.user
 
-                # remove verification_code and captcha field from form
-                cleaned_data.pop('verification_code', None)
-                cleaned_data.pop('captcha', None)
+            if active_form_class == FollowUpCheckupForm:
+                phone_number = form.cleaned_data.get('phone_number')
+                try:
 
-                FollowUpOutreach.objects.update_or_create(**cleaned_data)
-
-            else:
-                instance = form.save(commit=False)
-
-                # update form instance
-                if (
-                    active_form_class == FollowUpCheckupForm
-                    or active_form_class == FollowUpMembershipForm
-                ):
-                    instance.user = request.user
-
-                if active_form_class == FollowUpCheckupForm:
-                    # get member profile,
-                    # no need for checks as it is already
-                    # vetted in form clean
                     member_profile = MemberFollowUp.objects.filter(
                         phone_number=phone_number
                     ).first()
 
-                    # update instance
+                    # update indance
                     instance.member_profile = member_profile
-
-                    # update model instance of  member current welfare check
                     member_profile.current_status = instance.current_status
-                    member_profile.save(update_fields=['current_status'])
+                    instance.save(update_fields=['member_profile', 'current_status'])
 
-                # submit form
-                instance.save()
+                except MemberFollowUp.DoesNotExist:
+                    messages.error(request, "Could not find matching member profile.")
+                    context = self._get_context_data(
+                        **{form_key: form, 'form_section': form_section}
+                    )
+                    return render(request, self.template_name, context)
 
-            # message
+            instance.save()
+
             messages.success(request, message_text)
-            return HttpResponsePermanentRedirect(reverse_lazy(self.success_url))
+            return redirect(self.success_url_name)
 
+        else:
+            messages.error(request, 'Invalid Form Field(s). All fields are required.')
+            context = self._get_context_data(
+                **{form_key: form, 'form_section': form_section}
+            )
+            return render(request, self.template_name, context)
+
+
+class OutreachView(SuccessMessageMixin, CreateView):
+    model = FollowUpOutreach
+    form_class = OutreachForm
+    template_name = 'forms/outreach.html'
+    success_url = reverse_lazy('outreach')
+    success_message = 'Outreach Form Saved Successfully.'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return response
+
+    def form_invalid(self, form):
         # message
-        messages.error(request, 'Invalid Form Field(s). All fields are required.')
-
-        # render page with form errors
-        return render(
-            request,
-            self.template_name,
-            {
-                'checkup_form': FollowUpCheckupForm(),
-                'sunday_form': FollowUpMembershipForm(),
-                'outreach_form': FollowUpOutreachForm(),
-                'form': form,
-                'form_section': selected_form_section,
-            },
-        )
-
-    def dispatch(self, request, *args, **kwargs):
-        # check if user is authenticated
-        if self.request.user.is_anonymous:
-            return HttpResponsePermanentRedirect(reverse('account_login'))
-
-        return super().dispatch(request, *args, **kwargs)
+        print(form.errors)
+        messages.error(self.request, 'Invalid Form Field(s). All fields are required.')
+        return super().form_invalid(form)
